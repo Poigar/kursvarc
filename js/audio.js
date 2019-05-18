8ea7a05f9c0e79eeacc85fcc3d49d74d6b0871be
Reinards <reinardssaulitis@gmail.com>
2019-05-18 17:04:32 +0300

    Change audio path

--- js/audio.js
+++ js/audio.js
@@ -1,17 +1,17 @@
-var backgroundMusic = new Audio("../audio/bg_music.mp3");
+var backgroundMusic = new Audio("/audio/bg_music.mp3");
 backgroundMusic.volume = 0.3;
 backgroundMusic.loop = true;
 
-var clickSound = new Audio("../audio/click.wav");
+var clickSound = new Audio("/audio/click.wav");
 clickSound.volume = 0.4;
 
-var correctSound = new Audio("../audio/correct.mp3");
+var correctSound = new Audio("/audio/correct.mp3");
 correctSound.volume = 0.4;
 
-var wrongSound = new Audio("../audio/wrong.wav");
+var wrongSound = new Audio("/audio/wrong.wav");
 wrongSound.volume = 0.7;
 
-var levelupSound = new Audio("../audio/levelup.wav");
+var levelupSound = new Audio("/audio/levelup.wav");
 levelupSound.volume = 0.7;
 
 function playMusic(){
