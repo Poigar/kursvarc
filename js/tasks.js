97c80f38243f30b05e7f9326cfd7a581eef5262e
LaughingApe <niksderums@inbox.lv>
2019-05-18 12:12:06 +0300

    task file

---
+++ js/tasks.js
@@ -0,0 +1,77 @@
+var T = [
+    [ // 1 
+        ["vads", "vac", ""],
+        ["skaists", "skaisc", ""],
+        ["brilles", "briles", ""],
+        ["poga", "puaga", ""],
+        ["problēma", "proglēma", ""],
+        ["bungas", "bunkas", ""],
+        ["banka", "bannka", ""]
+        ["pie mums", "pie mumsīm"],
+        ["pie jums", "pie jumsīm"]
+    ],
+    [ // 2
+        ["gulta", "gūlta", ""],
+        ["bulta", "būlta", ""],
+        ["kūlenis", "kulenis", ""],
+        ["kultenis", "kūltenis", ""],
+        ["tualete", "tolete"],
+        ["tualete", "toalete"],
+        ["mežs", "mešs", ""],
+        ["kods", "kots", ""],
+        ["acis", "aces", ""],
+        ["acis", "ači", ""]
+    ],
+    [ // 3
+        ["pie manis", "pie manīm", ""],
+        ["pie tevis", "pie tevīm", ""],
+        ["nulle", "nule", "0"],
+        ["logs", "loks", ""]
+        ["numurs", "nummurs", ""],
+        ["treniņš", "trenniņš", ""],
+        ["treneris", "trenneris", ""]
+    ],
+    [ // 4
+        ["nākamais pasākums", "nākošais pasākums", ""],
+        ["košs", "kožs", ""],
+        ["pašreiz", "patreiz", ""]
+    ],
+    [ // 5
+        ["pulkstenis", "pulkstens", ""],
+        ["zibens", "zibenis", ""],
+        ["suns", "sunis", ""],
+        ["liktenis", "liktens", ""],
+        ["asmens", "asmenis", ""],
+        ["akmens", "akmenis", ""],
+        ["ūdens", "ūdenis", ""],
+    ],
+    [ // 6
+        ["Saeima", "Saima", ""],
+        ["Saeima", "Saema", ""],
+        ["programmēšana", "programēšana", ""],
+        ["flomāsters", "flomasters", ""]
+        ["spilgts", "spilkts", ""],
+        ["nule", "nulle", '"tikko"'],
+    ],
+    [ // 7
+        ["Pēterim Liepam", "Pēterim Liepai", "Kam? (Pēteris Liepa)"],
+        ["Tālim Eglem", "Tālim Eglei", "Kam? (Tālis Egle)"],
+        ["Augustam Oglem", "Augustam Oglei", "Kam? (Augusts Ogle)"],
+        ["Mikum Ulmanim", "Mikusam Ulmanim", "Kam? (Mikus Ulmanis)"],
+        ["Ingusam Kalniņam", "Ingum Kalniņam", "Kam? (Inguss Kalniņš)"],
+        ["mezdams", "mesdams", ""],
+        ["nesdams", "nesdams", ""],
+        ["vilkdams", "vilgdams", ""],
+        ["vezdams", "vesdams", ""],
+        ["laizdams", "laisdams", ""],
+        ["vests", "vezts", ""],
+        ["lauzts", "lausts", ""],
+        ["lauzdams", "lausdams", ""]
+    ],
+    [ // 8
+        ["fotografēt", "fotogrāfēt", ""],
+        ["fotogrāfija", "fotografija", ""],
+        ["Maijai Bunkui", "Maijai Bunkai", "Kam? (Maija Bunkus)"],
+        ["Annai Tellui", "Annai Tellusai", "Kam? (Anna Tellus)"]
+    ],
+];
